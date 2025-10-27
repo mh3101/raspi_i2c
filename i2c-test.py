9a2136c120ebb15b8dd64f228ff010cf35c40702
mh3101 <michael.haberern@web.de>
2025-10-27 17:17:51 +0100

    Konfigurationsports eingerichtet

--- i2c-test.py
+++ i2c-test.py
@@ -3,21 +3,25 @@
 from smbus2 import SMBus
 import time
 
-DEVICE_ADDRESS = 0x20      #7 bit address (will be left shifted to add the read write bit)
-DEVICE_REG_MODE1 = 0x00
+DEVICE_ADDRESS = 0x20      # 7 bit address (will be left shifted to add the read write bit)
+DEVICE_REG_MODE1 = 0x06    # Konfigurationsregister
+DEVICE_MODE_OUT = 0x00     # Konfgurationsregister -> Ausgabe
+DEVICE_REG_OUT1 = 0x02     # Ausgaberegister
 
 status = 1
 
 bus = SMBus(1)
 
+bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_MODE_OUT)
+
 # --- Hauptprogramm ---
 try:
     while True:
         if status:
-            bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, 0x00)
+            bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_OUT1, 0x00)
             print("Relais aus")
         else:
-            bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, 0x255)
+            bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_OUT1, 0x255)
             print("Relais ein")
                 
         status ^= 1   
@@ -26,6 +30,6 @@ try:
 except KeyboardInterrupt:
     print("\nBeendet vom Benutzer.")
     # --- Aufräumen ---
-    bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, 0x00)
+    bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_OUT1, 0x00)
     bus.close()
     print("bus freigegeben, Script beendet.")
